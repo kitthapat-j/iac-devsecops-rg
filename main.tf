2b2e7d786aaefe04dd462d947fc8e15c81db8998
kitthapat <admin@contoso.com>
2025-10-21 14:19:30 +0000

    Initial Commit : Azure Resource Group and Insecure NSG

--- main.tf
+++ main.tf
@@ -9,6 +9,17 @@ resource "azurerm_network_security_group" "example_nsg" {
     resource_group_name = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_resource_group_lock" "rg_lock" {
+  // ตั้งชื่อ Lock
+  name                = "rg-do-not-delete-lock"
+  // เลือกระดับการ Lock: CanNotDelete หรือ ReadOnly
+  lock_level          = "CanNotDelete"
+  // เชื่อมโยงกับ Resource Group ที่มีอยู่
+  resource_group_name = azurerm_resource_group.rg.name
+  
+  notes               = "Prevents accidental deletion of this critical Resource Group."
+}
+
 
 resource "azurerm_network_security_rule" "insecure_ssh" {
     name                        = "Allow-SSH-Public"
@@ -18,7 +29,7 @@ resource "azurerm_network_security_rule" "insecure_ssh" {
     protocol                    = "Tcp"
     source_port_range           = "*"
     destination_port_range      = "22"
-    source_address_prefix       = "10.0.0.0/24"
+    source_address_prefix       = "10.0.0.4/32"
     destination_address_prefix  = "*"
     network_security_group_name = azurerm_network_security_group.example_nsg.name
     resource_group_name         = azurerm_resource_group.rg.name
